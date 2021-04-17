5cb76f0db98afea563bea2ab22a960c2989dff0f
Hy Nguyen <hynguyen2727@gmail.com>
2021-04-17 17:03:02 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:list_images/my_app.dart';
 
 void main(List<String> args) {
+  print('hello');
   runApp(
     MaterialApp(
       home: MyApp(),
